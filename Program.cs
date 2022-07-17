2860a70f51e0d73e01d3596084805d6c814a03c0
goyal123 <akhilg642@gmail.com>
2022-07-17 18:53:13 +0530

    Adding functionality of count of number of times dice roll to win and position of player after every dice rolled

--- Program.cs
+++ Program.cs
@@ -4,13 +4,14 @@
     {
         static void Main(string[] args)
         {
-            int player_pos = 0,dice_num,options,sum=0;
+            int player_pos = 0,dice_num,options,sum=0,count=0;
             Console.WriteLine("Welcome to Snake Ladder Game!");
             Random rand=new Random();
                     
             while (player_pos != 100)
             {
                 dice_num = rand.Next(1, 6);
+                count += 1;
                 Console.WriteLine("Number on dice = " + dice_num);
                 options = rand.Next(0, 3);
                 Console.WriteLine("Choosing option " + options);
@@ -49,6 +50,7 @@
                         break;
                 }
             }
+            Console.WriteLine("No of times dice played to win = " + count);
 
 
 
