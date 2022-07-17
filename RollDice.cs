7d3aecd0ebc10cbf10fc34a9ba33c33b5a990da9
goyal123 <akhilg642@gmail.com>
2022-07-17 20:25:37 +0530

    Rolling Dice Functionality

---
+++ RollDice.cs
@@ -0,0 +1,20 @@
+﻿namespace SnakeLadder
+{
+    public class RollDice
+    {
+        public static int Dice_Roll()
+        {
+            int dice_num;
+            Random rand = new Random();
+            dice_num = rand.Next(1, 6);
+            return dice_num;
+        }
+        public static int Roll_Options()
+        {
+            int options;
+            Random rand = new Random();
+            options = rand.Next(0, 3);
+            return options;
+        }
+    }
+}
