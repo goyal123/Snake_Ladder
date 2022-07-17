4eda750ae6a6645d864f8f927130e28e0ecf85f4
goyal123 <akhilg642@gmail.com>
2022-07-17 18:06:18 +0530

    Rollice Dice Functionality

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
-            int player_pos = 0;
+            int player_pos = 0,dice_num;
             Console.WriteLine("Welcome to Snake Ladder Game!");
+            Random rand=new Random();
+            dice_num=rand.Next(1, 6);
+            Console.WriteLine(dice_num);
+
 
         }
     }
