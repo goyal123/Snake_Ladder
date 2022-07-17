7ec6d5b05c5382071615b19f65bdf4d6e356e358
goyal123 <akhilg642@gmail.com>
2022-07-17 18:42:54 +0530

    Adding functionality of player reaching the 100 using while loop

--- Program.cs
+++ Program.cs
@@ -7,42 +7,47 @@
             int player_pos = 0,dice_num,options,sum=0;
             Console.WriteLine("Welcome to Snake Ladder Game!");
             Random rand=new Random();
-            dice_num=rand.Next(1, 6);
-            Console.WriteLine("Number on dice = "+dice_num);
-            options = rand.Next(0, 3);
-            Console.WriteLine("Choosing option " + options);
-            switch(options)
+                    
+            while (player_pos != 100)
             {
-                case 0:
-                    sum = player_pos;
-                    Console.WriteLine("Player position = " + player_pos);
-                    break;
-                case 1:
-                    if((sum+dice_num)>100)
-                    {
-                        sum = player_pos;
-                    }
-                    if((sum+dice_num)<100)
-                    {
-                        sum=sum+dice_num;
-                        player_pos = sum;
-                    }
-                    Console.WriteLine("Player position = " + player_pos);
-                    break;
-                case 2:
-                    if((sum-dice_num)<0)
-                    {
-                        sum = 0;
-                        player_pos = 0;
+                dice_num = rand.Next(1, 6);
+                Console.WriteLine("Number on dice = " + dice_num);
+                options = rand.Next(0, 3);
+                Console.WriteLine("Choosing option " + options);
+                switch (options)
+                {
+                    case 0:
+                        player_pos = sum; ;
+                        Console.WriteLine("Player position = " + player_pos);
+                        break;
 
-                    }
-                    else
-                    {
-                        sum = sum - dice_num;
-                        player_pos = sum;
-                    }
-                    Console.WriteLine("Player position = " + player_pos);
-                    break;
+                    case 1:
+                        if ((sum + dice_num) > 100)
+                            sum = player_pos;
+
+                        if ((sum + dice_num) < 100)
+                        {
+                            sum = sum + dice_num;
+                            player_pos = sum;
+                        }
+                        Console.WriteLine("Player position = " + player_pos);
+                        break;
+
+                    case 2:
+                        if ((sum - dice_num) < 0)
+                        {
+                            sum = 0;
+                            player_pos = 0;
+
+                        }
+                        else
+                        {
+                            sum = sum - dice_num;
+                            player_pos = sum;
+                        }
+                        Console.WriteLine("Player position = " + player_pos);
+                        break;
+                }
             }
 
 
